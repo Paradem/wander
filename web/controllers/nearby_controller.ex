464adbc7d7cf4fc2e68e934fc41ecb5337402971
Kilian Cirera Sant <kilian@paradem.co>
2016-05-05 19:28:45 -0400

    Refactor nearby controller

--- web/controllers/nearby_controller.ex
+++ web/controllers/nearby_controller.ex
@@ -8,43 +8,39 @@ defmodule Wander.NearbyController do
 
   @nearby_distance {0.5, :km}
 
-  def index(conn, %{"lat" => lat, "lng" => long}) do
-    response = %Point{y: String.to_float(lat), x: String.to_float(long)}
-    |> nearby
-
-    json conn, response
+  def index(conn, params) do
+    json conn, params |> as_point |> nearby
   end
 
-  defp nearby(%Point{} = point) do
-    {response, _ } = {%{}, point}
-    |> with_q
+  defp nearby(point) do
+    %{q: %{lat: point.y, lng: point.x}}
     |> with_locations
     |> with_city
     #|> with_neighborhoods
     #|> with_curated_collections
-    response
   end
 
-  defp with_q({response, point}) do
-    {response |> Map.merge(%{q: %{lat: point.y, lng: point.x}}), point}
-  end
-
-  defp with_locations({response, point}) do
+  defp with_locations(%{q: point} = response) do
     locations = Location
-    |> Distance.within_distance(@nearby_distance, point)
+    |> Distance.within_distance(@nearby_distance, as_point(point))
     |> Repo.all
     |> Location.as_backwards_compatible
 
-    {response |> Map.merge(%{locations: locations}), point}
+    response |> Map.merge(%{locations: locations})
   end
 
-  defp with_city({%{locations: []}, _} = input), do: input
+  defp with_city(%{locations: []} = response), do: response
 
-  defp with_city({%{locations: locations} = response, point}) do
+  defp with_city(%{locations: locations, q: point} = response) do
     city = (from city in City, where: city.id == ^hd(locations).city_id)
     |> Repo.one!
-    |> City.with_geofences(@nearby_distance, point)
+    |> City.with_geofences(@nearby_distance, as_point(point))
 
-    {response |> Map.merge(%{city: city}), point}
+    response |> Map.merge(%{city: city})
+  end
+
+  defp as_point(%{lat: lat, lng: lng}), do: %Point{y: lat, x: lng}
+  defp as_point(%{"lat" => lat, "lng" => lng}) do
+    %Point{y: String.to_float(lat), x: String.to_float(lng)}
   end
 end
